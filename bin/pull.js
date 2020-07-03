39822614a561d5f62bae20b727a84cd4ad3aa0b2
zw <catherine_8917@163.com>
2020-07-03 10:11:15 +0800

    update

--- bin/pull.js
+++ bin/pull.js
@@ -1,17 +1,21 @@
 var zmq = require("zmq");
 
-const pull_addr = "tcp://172.31.47.199:5555";
+const pull_addr = `tcp://172.31.7.119:${process.argv[2]}`;
+const message_count = process.argv[3];
 
 let socket = zmq.createSocket("pull");
 socket.connect(pull_addr);
 let echo = zmq.createSocket("push");
-echo.bindSync("tcp://*:5556");
+echo.bindSync("tcp://*:5560");
 let counter = 0;
 socket.on("message", () => {
     ++counter;
     // console.log(counter);
-    echo.send(counter);
-})
-socket.on("close", () => {
-    console.log("push close");
+    if(counter == message_count) {
+        echo.send(counter);
+        socket.close();
+        echo.close();
+    }else {
+        echo.send(counter);
+    }
 })
--- bin/push.js
+++ bin/push.js
@@ -1,39 +1,91 @@
 var zmq = require("zmq");
-var os = require("os");
+const cluster = require("cluster");
 
 var message_size = Number(process.argv[2]);
 var message_count = Number(process.argv[3]);
 var message = new Buffer(message_size);
 message.fill("h");
 
-let push = zmq.createSocket("push");
-push.bindSync("tcp://*:5555");
 let operation = 0;
-const pull_ip = ["172.31.44.3", "172.31.44.50","172.31.34.25", "172.31.46.108"];
-let sockets = [];
-for (let i = 0; i < pull_ip.length; i++) {
-  let echo = zmq.createSocket("pull");
-//   console.log(`tcp://${pull_ip[i]}:5556`);
-  echo.connect(`tcp://${pull_ip[i]}:5556`);
-  sockets.push(echo);
-}
-for (let socket of sockets) {
-    socket.on("message", () => {
-        ++operation;
-        if (operation == message_count) {
-            let endtime = process.hrtime(timer);
-            let sec = endtime[0] + endtime[1] / 1000000000;
-            let throughput = message_count / sec;
-            console.log("message size: %d [B]", message_size);
-            console.log("message count: %d", message_count);
-            console.log("mean throughput: %d [ops/s]", throughput);
-            console.log("overall time: %d secs", sec);
-            push.close();
-            socket.close();
+const pull_ip = ["172.31.7.152"];
+const push_port = ["5555"];
+const pull_port = 5560;
+let pull_sockets = [];
+let ops = Number(message_count / push_port.length);
+
+// Create sockets for receving messages
+// for (let i = 0; i < pull_ip.length; i++) {
+//   let pull = zmq.createSocket("pull");
+//   pull.connect(`tcp://${pull_ip[i]}:${pull_port}`);
+//   pull_sockets.push(pull);
+// }
+// for (let pull of pull_sockets) {
+//   console.log(pull);
+//   // pull.on("message", (count) => {
+//   //   ++operation;
+//   //   console.log(operation);
+//   //   if (count == ops) {
+//   //     let endtime = process.hrtime(timer);
+//   //     let sec = endtime[0] + endtime[1] / 1000000000;
+//   //     let throughput = ops / sec;
+//   //     console.log("message size: %d [B]", message_size);
+//   //     console.log("message count: %d", message_count);
+//   //     console.log("mean throughput: %d [ops/s]", throughput);
+//   //     console.log("overall time: %d secs", sec);
+//   //     pull.close();
+//   //     // push.close();
+//   //   }
+//   // });
+// }
+
+
+
+if (cluster.isMaster) {
+  for (let i = 0; i < push_port.length; i++) {
+    let worker = cluster.fork();
+    worker.send({port:push_port[i], index:i});
+    // cluster.on("message", (worker, message, handle) => {
+    //   operation_num += message.ops;
+    //   total_time += message.sec;
+    //   ++worker_num;
+    //   // console.log(`operation number: ${operation_num}`);
+    //   // console.log(`total time: ${total_time}`);
+    //   // console.log(`work number: ${worker_num}`);
+    //   if (worker_num == push_port.length) {
+    //       let throughput = operation_num / total_time;
+    //       console.log("message size: %d [B]", message_size);
+    //       console.log("message count: %d", message_count);
+    //       console.log("mean throughput: %d [ops/s]", throughput);
+    //       console.log("overall time: %d secs", total_time);
+    //     }
+    // });
+  }
+} else {
+  process.on("message", ({port, index}) => {
+    let push = zmq.createSocket("push");
+    push.bindSync(`tcp://*:${port}`);
+    let pull = zmq.createSocket("pull");
+    pull.connect(`tcp://${pull_ip[index]}:${pull_port}`);
+    let operation = 0;
+    // console.log(pull_sockets[index]);
+    pull.on("message", () => {
+      ++operation;
+      if(operation == ops) {
+        let endtime = process.hrtime(timer);
+        let sec = endtime[0] + endtime[1] / 1000000000;
+        let throughput = ops / sec;
+        console.log("message size: %d [B]", message_size);
+        console.log("message count: %d", message_count);
+        console.log("mean throughput: %d [ops/s]", throughput);
+        console.log("overall time: %d secs", sec);
+        pull.close();
+        push.close();
+      }
+    });
+    let timer = process.hrtime();
+    for(let i = 0; i < ops; i++) {
+      push.send(message);
     }
-  });
-}
-let timer = process.hrtime();
-for (var i = 0; i < message_count; i++) {
-    push.send(message);
+    
+  })
 }
